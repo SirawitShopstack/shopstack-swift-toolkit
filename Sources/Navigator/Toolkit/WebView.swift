2c3181900dde29f8215d7d87486bdf0c13126314
Sirawit Laoviriyakul <sirawit@shopstack.asia>
2025-01-20 16:55:41 +0700

    fixbug

--- Sources/Navigator/Toolkit/WebView.swift
+++ Sources/Navigator/Toolkit/WebView.swift
@@ -19,13 +19,13 @@ final class WebView: WKWebView {
         let config = WKWebViewConfiguration()
         config.mediaTypesRequiringUserActionForPlayback = .all
 
-         // Disable the Apple Intelligence Writing tools in the web views.
+       // Disable the Apple Intelligence Writing tools in the web views.
         // See https://github.com/readium/swift-toolkit/issues/509#issuecomment-2577780749
-        if #available(iOS 18.0, *) {
-        if let behavior = UITextView.WritingToolsBehavior.self as? UITextView.WritingToolsBehavior.Type {
-            behavior.writingToolsBehavior = .none
-        }
-    }
+        #if compiler(>=6.0)
+            if #available(iOS 18.0, *) {
+                config.writingToolsBehavior = .none
+            }
+        #endif
         
         super.init(frame: .zero, configuration: config)
 
